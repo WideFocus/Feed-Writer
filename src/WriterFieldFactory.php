2962bc38ed4362b00c6febc9432897c4d34d6fc1
Ashoka de Wit <sjokki@gmail.com>
2016-12-25 18:10:30 +0800

    Create WriterFieldFactory implementation

---
+++ src/WriterFieldFactory.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright WideFocus. All rights reserved.
+ * http://www.widefocus.net
+ */
+
+namespace WideFocus\Feed\Writer;
+
+/**
+ * Creates writer fields.
+ */
+class WriterFieldFactory implements WriterFieldFactoryInterface
+{
+    /**
+     * Create a writer field.
+     *
+     * @param string   $name
+     * @param string   $label
+     * @param callable $filter
+     *
+     * @return WriterFieldInterface
+     */
+    public function createField(
+        string $name,
+        string $label,
+        callable $filter = null
+    ): WriterFieldInterface {
+        return new WriterField($name, $label, $filter);
+    }
+}
---
+++ tests/WriterFieldFactoryTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright WideFocus. All rights reserved.
+ * http://www.widefocus.net
+ */
+
+namespace WideFocus\Feed\Writer\Tests;
+
+use PHPUnit_Framework_TestCase;
+use WideFocus\Feed\Writer\WriterFieldFactory;
+use WideFocus\Feed\Writer\WriterFieldInterface;
+
+/**
+ * @covers \WideFocus\Feed\Writer\WriterFieldFactory
+ */
+class WriterFieldFactoryTest extends PHPUnit_Framework_TestCase
+{
+    use CommonMocksTrait;
+
+    /**
+     * @param string   $name
+     * @param string   $label
+     * @param callable $filter
+     *
+     * @return WriterFieldInterface
+     *
+     * @covers \WideFocus\Feed\Writer\WriterFieldFactory::createField
+     *
+     * @dataProvider fieldDataProvider
+     */
+    public function testCreateField(string $name, string $label, callable $filter = null): WriterFieldInterface
+    {
+        $factory = new WriterFieldFactory();
+        return $factory->createField($name, $label, $filter);
+    }
+
+    /**
+     * @return array
+     */
+    public function fieldDataProvider(): array
+    {
+        return [
+            [
+                'name',
+                'label'
+            ],
+            [
+                'another_name',
+                'another_label',
+                $this->createFilterMock()
+            ]
+        ];
+    }
+}
